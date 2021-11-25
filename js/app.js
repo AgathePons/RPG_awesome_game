744c6b2b2647147eb8956bd3071c3c84993e288f
AgathePons <ponsagathe37@gmail.com>
2021-11-25 17:20:51 +0100

    adding player div and targetCell

--- js/app.js
+++ js/app.js
@@ -14,22 +14,36 @@ var app = {
     app.drawBoard(4, 6);
   },
 
+  /**
+   * function to build the board
+   * @param {number} nbOfRows 
+   * @param {number} nbOfCells 
+   */
   drawBoard: function (nbOfRows, nbOfCells) {
-    console.log('tadaaa');
     for (var iRows = 0; iRows < nbOfRows; iRows++) {
       var row = document.createElement('div');
       row.classList.add('row');
 
       for (var iCells = 0; iCells < nbOfCells; iCells++) {
-        console.log(iCells);
         var cell = document.createElement('div');
         cell.classList.add('cell');
+        cell.id = `x-${iCells}-y-${iRows}`;
+
+        // check cells coordinates to position targetCell and player div
+        if (cell.id === `x-${app.targetCell.positionX}-y-${app.targetCell.positionY}`) {
+          cell.classList.add('targetCell');
+        } else if (cell.id === `x-${app.player.positionX}-y-${app.player.positionY}`) {
+          var playerDiv = document.createElement('div');
+          playerDiv.classList.add('player');
+          cell.appendChild(playerDiv);
+        }
+
         row.appendChild(cell);
       }
 
       app.board.appendChild(row);
     }
-  }
+  },
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
