22475bea0f19bfc46109b478dce5cbdab931f441
AgathePons <ponsagathe37@gmail.com>
2021-11-25 16:34:25 +0100

    preparer le terrain done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 var app = {
+  player: {
+    positionX: 0,
+    positionY: 0,
+    direction: 'right',
+  },
+  targetCell: {
+    positionX: 5,
+    positionY: 3,
+  },
   init: function () {
     console.log('init !');
-  }
+  },
+  
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
