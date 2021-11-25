7daff8aa66749d909d77d6c79b89bfb610daebd3
AgathePons <ponsagathe37@gmail.com>
2021-11-25 16:53:19 +0100

    adding reset.css + build the grid + style the grid

--- js/app.js
+++ js/app.js
@@ -8,10 +8,28 @@ var app = {
     positionX: 5,
     positionY: 3,
   },
+  board: document.getElementById('board'),
+
   init: function () {
-    console.log('init !');
+    app.drawBoard(4, 6);
   },
-  
+
+  drawBoard: function (nbOfRows, nbOfCells) {
+    console.log('tadaaa');
+    for (var iRows = 0; iRows < nbOfRows; iRows++) {
+      var row = document.createElement('div');
+      row.classList.add('row');
+
+      for (var iCells = 0; iCells < nbOfCells; iCells++) {
+        console.log(iCells);
+        var cell = document.createElement('div');
+        cell.classList.add('cell');
+        row.appendChild(cell);
+      }
+
+      app.board.appendChild(row);
+    }
+  }
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
